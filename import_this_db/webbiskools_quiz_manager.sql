ae1559225421cde18c2a3d9b71eafe6f1557a517
taransandhur1 <99458148+taransandhur1@users.noreply.github.com>
2022-02-11 03:52:35 +0000

    Update webbiskools_quiz_manager.sql

--- import_this_db/webbiskools_quiz_manager.sql
+++ import_this_db/webbiskools_quiz_manager.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 14, 2020 at 06:13 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
